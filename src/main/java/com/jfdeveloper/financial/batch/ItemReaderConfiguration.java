6497afaa6c7ff4148071437dba63cd31273f0d50
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2022-04-17 06:35:52 -0400

    working on Batch processing

--- src/main/java/com/jfdeveloper/financial/batch/ItemReaderConfiguration.java
+++ src/main/java/com/jfdeveloper/financial/batch/ItemReaderConfiguration.java
@@ -3,6 +3,9 @@ package com.jfdeveloper.financial.batch;
 import com.jfdeveloper.financial.entities.Dataset;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -10,11 +13,27 @@ import javax.sql.DataSource;
 @Configuration
 public class ItemReaderConfiguration {
 
+  @Bean
   public ItemReader<Dataset> itemReader(DataSource dataSource){
-	JdbcPagingItemReader<Dataset>
+
+      JdbcPagingItemReader<Dataset> jdbcPagingItemReader=new JdbcPagingItemReader<>();
+      jdbcPagingItemReader.setDataSource(dataSource);
+      jdbcPagingItemReader.setPageSize(1000);
+
+
+
+      return null;
+  }
+  private PagingQueryProvider createQuery(){
+      MySqlPagingQueryProvider queryProvider=new MySqlPagingQueryProvider();
+      queryProvider.setSelectClause("SELECT *");
+      queryProvider.setFromClause("FROM DATASET");
+
+
+      return null;
   }
 
-
+  private Map<String>
 
 
 
