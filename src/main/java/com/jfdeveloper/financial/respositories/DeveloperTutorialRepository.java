0b99384449225f283fd74d0abc23ae5613c6be67
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-04-19 01:39:59 -0400

    removed old/not used code. added working code. need to refactor for mysql

--- src/main/java/com/jfdeveloper/financial/respositories/DeveloperTutorialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jfdeveloper.financial.respositories;
-
-import com.jfdeveloper.financial.entities.DeveloperTutorial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeveloperTutorialRepository extends JpaRepository<DeveloperTutorial, Long> {
-}
